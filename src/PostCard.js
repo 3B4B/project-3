598821c0bff206e93407e6ae6436dfbdb1fa0bc0
Francesca Ansell <fla5029@psu.edu>
2021-11-10 21:39:27 -0500

    updates

--- src/PostCard.js
+++ src/PostCard.js
@@ -12,25 +12,19 @@ export class PostCard extends LitElement {
   // HTMLElement life-cycle, built in; use this for setting defaults
   constructor() {
     super();
-    this.need = 'all need to succeed';
+    this.label = 'P O S T    C A R D';
   }
 
   // properties that you wish to use as data in HTML, CSS, and the updated life-cycle
   static get properties() {
     return {
-      need: { type: String, reflect: true },
+      label: { type: String, reflect: true },
     };
   }
 
   // updated fires every time a property defined above changes
   // this allows you to react to variables changing and use javascript to perform logic
-  updated(changedProperties) {
-    changedProperties.forEach((oldValue, propName) => {
-      if (propName === 'need' && this[propName] === 'joy') {
-        this.classList.add('joyful');
-      }
-    });
-  }
+  updated() {}
 
   // Lit life-cycle; this fires the 1st time the element is rendered on the screen
   // this is a sign it is safe to make calls to this.shadowRoot
@@ -57,20 +51,87 @@ export class PostCard extends LitElement {
     return css`
       :host {
         display: block;
+        background-color: #f6f0e8;
+        border: 1px solid grey;
+        border-radius: 4;
+        box-shadow: 3px 3px 3px grey;
+        margin: 20px;
+        height: 300px;
+        width: 700px;
+        text-align: center;
+        display: inline-grid;
+        grid-template-rows: 0.5fr 1fr 1fr 1fr 0.25fr;
+        grid-template-columns: 1fr 1fr 0.2fr 1fr 1fr;
       }
-      :host([need='joy']) {
-        color: yellow;
-        background-color: black;
+      .header {
+        grid-column: 1 / span 5;
+        grid-row: 1;
+      }
+      .postage {
+        grid-column: 4;
+        grid-row: 1 / 3;
+      }
+      .image {
+        grid-column: 1 / 3;
+        grid-row: 2 / 4;
+
+        border-radius: 1px;
+      }
+      .stamp {
+        grid-column: 5;
+        grid-row: 1 / 3;
+      }
+      .tofrom {
+        grid-column: 4 /6;
+        grid-row: 3 / 5;
+      }
+
+      .message {
+        grid-column: 1 / 3;
+        grid-row: 4;
+      }
+      .line {
+        grid-column: 3;
+        grid-row: 2 / 5;
+        background: linear-gradient(#000, #000) no-repeat center/2px 100%;
+      }
+
+      div {
+        border: 2px dotted purple;
+        padding: 10px;
       }
     `;
   }
 
-  // HTML - specific to Lit
+  /*
+  Will need to use Z index for layering 
+  */
   render() {
     return html`
-      <h1>Make me awesome</h1>
-      <p>Build the future we ${this.need}.</p>
-      <slot></slot>
+        <div class="header">
+            ${this.label}
+        </div>
+        <div class="postage">
+ 
+        </div>
+        <div class="image">
+     
+        </div>
+        <div class="stamp">
+    
+        </div>
+        <div class="tofrom">
+          TO: <br>
+          <slot name="to"></slot>
+          FROM: <br>
+          <slot name="from"></slot>
+        </div>
+        <div class="message">
+          <slot name="message"></slot>
+        </div>
+        <div class="line"></div>
+      </div>
+
     `;
   }
 
